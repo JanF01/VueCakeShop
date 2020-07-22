bcffa139542794ccc5bcd61697f5787ec9fe477b
JanF01 <jan.fiolka44@gmail.com>
2020-07-22 22:11:08 +0200

    Project initialization

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <navigation>
+  </navigation>
+</template>
+
+<script>
+import Navigation from './components/Navigation'
+
+export default {
+  name: "App",
+  components: {
+   Navigation
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+   <h3>
+    Welcome to Cake Shop
+   </h3>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navigation",
+  props: {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+nav{
+  width:100%;
+  h3{
+    margin: 0 auto;
+    width:20%;
+    font-size:1.3em;
+    color:darkgrey;
+    font-family:Sans-serif;
+  }
+}
+</style>
